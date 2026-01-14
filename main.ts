1e52d1d45723fdeeb3381dcf6cea2be67a287b70
noahmagnin8-lab <noahmagnin8@gmail.com>
2026-01-14 14:54:36 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,10 @@
 basic.forever(function () {
     music.play(music.stringPlayable("- - - - - - - - ", 120), music.PlaybackMode.UntilDone)
     motors.dualMotorPower(Motor.M0, 100)
+    led.toggle(0, 0)
+    if (true) {
+        led.toggle(0, 0)
+    } else {
+        led.unplot(0, 0)
+    }
 })
