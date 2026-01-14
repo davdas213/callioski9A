0436b8bd438e2ed00ff0811c2e81486046e3867c
Ruisu6654 <ruisu.guruba@gmx.de>
2026-01-14 14:59:09 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,3 @@
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
 })
-input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
-    music.play(music.stringPlayable("- - - - - - - - ", 120), music.PlaybackMode.UntilDone)
-})
