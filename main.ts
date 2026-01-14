ed4201c43bf42f79f3506039581a392cf657f97e
Ruisu6654 <ruisu.guruba@gmx.de>
2026-01-14 15:01:06 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,1 @@
-input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
-    music.play(music.tonePlayable(262, music.beat(BeatFraction.Whole)), music.PlaybackMode.UntilDone)
-})
-input.onGesture(Gesture.Shake, function () {
-	
-})
-basic.forever(function () {
-    if (true) {
-    	
-    }
-})
+
