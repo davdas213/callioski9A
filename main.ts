0dc193b91174eb25019f46341f1becf7810fc106
davdas213 <star.citicen27@gmail.com>
2026-01-14 14:56:20 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,3 @@
-basic.forever(function () {
+input.onPinTouchEvent(TouchPin.P0, input.buttonEventDown(), function () {
     music.play(music.stringPlayable("- - - - - - - - ", 120), music.PlaybackMode.UntilDone)
-    motors.dualMotorPower(Motor.M0, 100)
-    led.toggle(0, 0)
-    if (true) {
-        led.toggle(0, 0)
-    } else {
-        led.unplot(0, 0)
-    }
 })
